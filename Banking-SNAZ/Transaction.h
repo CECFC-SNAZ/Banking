5fa599d4afafe14c3440cb8aa9649664e637d2d0
Nicholas Pie <nicholas.pie@cecstudents.org>
2020-03-04 10:40:01 -0700

    Removed readEntireFile() because of errors and improved test()

--- Banking-SNAZ/Transaction.h
+++ Banking-SNAZ/Transaction.h
@@ -33,8 +33,6 @@ public:
 	bool setAcctID(int);                                // ^
 	void readFromFile(fstream&);
 	void readFromFile(ifstream&);
-	vector<Transaction> readEntireFile(fstream&);  // Returns a vector containing every transaction stored in the specified file
-	vector<Transaction> readEntireFile(ifstream&); // ^
 	bool storeInFile(fstream&);  // Returns true if file already existed, returns false if not
 	bool storeInFile(ofstream&); // ^
 	bool test(); // The test that is run at the beginning of main(), returns false in the event of an error
