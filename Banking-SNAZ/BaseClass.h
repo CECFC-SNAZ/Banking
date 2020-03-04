bc632781652743f0e466bc5cd3aec56188564619
Sedona Dionne <sedonadionne@gmail.com>
2020-03-04 10:27:57 -0700

    Added fees and withdrawal limit.

--- Banking-SNAZ/BaseClass.h
+++ Banking-SNAZ/BaseClass.h
@@ -2,7 +2,7 @@
 /*BaseClass.h
 by Sedona Dionne
 February 28, 2020
-Struct stores time of entry.  Class Base includes interest and fees.
+Struct stores time of entry.  Base Class includes fees, interest rate, and a withdrawal limit.
 */
 
 #include "Transaction.h"
@@ -14,22 +14,24 @@ struct entrystruct {
 class Base {
 private:
     double balance;
-    double depositfees, checkfees, overdraftfee, totalfees;
+    double depositfees, checkfees, overdraftfee, latefee, earlywithdrawfee, totalfees;
     entrystruct entryTime;
 public:
-    Base() { depositfees = 0, checkfees = 0, overdraftfee = 0, totalfees = 0;}
+    Base() { depositfees = 0, checkfees = 0, overdraftfee = 0, latefee = 0, earlywithdrawfee = 0, totalfees = 0; }
     Base(double bal);
     ~Base() {}
     double get_balance() const;
     void calcDeposit(double);
     void calcCheck(double);
     void calcOverdraft(double);
+    void calcLate(double);
+    void calcEarlywithdraw(double);
     void calcMonthInt();
     static float annualInterestRate;
     static void modifyIntRate(float value);
     void displayFee();
     void setEntryTime(entrystruct enteredtime) { entryTime = enteredtime; }
    entrystruct getEntryTime() { return entryTime; }
+   const int withdrawLimit = 500;
 };
-
 
