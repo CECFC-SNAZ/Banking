8f106e1b49374ac26f01d3ac2dcacf8228c90e0d
Nicholas Pie <nicholas.pie@cecstudents.org>
2020-03-05 10:45:10 -0700

    Added a test for getTime(), and finally wrote getTime()

--- Banking-SNAZ/Source.cpp
+++ Banking-SNAZ/Source.cpp
@@ -2,9 +2,12 @@
 // Team SNAZ
 // 2 March 2020
 
+
 #include "Transaction.h"
 #include "BaseClass.h"
 #include <iostream>
+#include "Cls.h"
+#include <string>
 using namespace std;
 
 const bool debug = true;
@@ -14,7 +17,13 @@ int main() {
 		Transaction tmpTrans;
 		if (not tmpTrans.test()) cout << "Error with transaction file I/O\n";
 	}
-
-
+	while (false) {
+		timeStruct tmpTime;
+		tmpTime = getTime();
+		cout << tmpTime.month << "/" << tmpTime.day << "/" << tmpTime.year << " " << tmpTime.hour << ":" << tmpTime.minute << ":" << tmpTime.second;
+		string tmpStr;
+		cin.clear();
+		getline(cin, tmpStr);
+	}
 	return 0;
 }
