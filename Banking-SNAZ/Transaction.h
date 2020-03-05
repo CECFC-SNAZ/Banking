ad79389b30dec928276288d8a9a258dbdf358cfc
Nicholas Pie <nicholas.pie@cecstudents.org>
2020-03-05 10:18:53 -0700

    Added Checking.cpp&.h, and Cls.cpp&.h Also made minor changes to Transaction.cpp&.h

--- Banking-SNAZ/Transaction.h
+++ Banking-SNAZ/Transaction.h
@@ -15,6 +15,8 @@ struct timeStruct {
 	int year = -1, month = -1, day = -1, hour = -1, minute = -1, second = -1;
 };
 
+timeStruct getTime();
+
 class Transaction {
 public:
 	enum class Type { Deposit, Withdrawal };
