bb9d4ac0c062b3b15e197555a343c0add731c957
Sedona11 <61513452+Sedona11@users.noreply.github.com>
2020-03-06 10:06:03 -0700

    Update BaseClass.cpp

--- Banking-SNAZ/BaseClass.cpp
+++ Banking-SNAZ/BaseClass.cpp
@@ -1,1 +1,41 @@
+/*BaseClass.cpp
+Sedona Dionne
+March 4, 2020
+*/
 
+#include "BaseClass.h"
+#include <iostream>
+
+
+  Base::Base() {
+entrytime = { 0, 0, 0, 0, 0 };
+}
+
+Base::Base() {
+  double bal = balance;
+}
+
+
+void Base::calcDeposit()
+{
+}
+
+void Base::calcCheck()
+{
+}
+
+void Base::calcLate()
+{
+}
+
+void Base::calcEarlywithdraw()
+{
+}
+
+void Base::calcMonthInt()
+{
+}
+
+void Base::displayFee()
+{
+}
