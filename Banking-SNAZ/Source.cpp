1d69ed5fd77fd37a96a6e9af72bf717c7b3c27fe
Nicholas Pie <nicholas.pie@cecstudents.org>
2020-03-24 11:53:45 -0600

    Commented out a line that was meant for testing and doesn't belong in the final version

--- Banking-SNAZ/Source.cpp
+++ Banking-SNAZ/Source.cpp
@@ -18,9 +18,9 @@ int main() {
 		Transaction tmpTrans;
 		if (not tmpTrans.test()) cout << "Error with transaction file I/O\n";
 	}
-	Transaction transaction;
+	/*Transaction transaction;
 
-	cout << "\n\n\n" << sizeof(transaction) << "\n\n\n";
+	cout << "\n\n\n" << sizeof(transaction) << "\n\n\n";*/
 
 
 	bool valid = true, exit = false;
